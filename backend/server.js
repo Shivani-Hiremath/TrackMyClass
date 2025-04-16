84a258e4e3f0dd519eb7c1bf15614ceaa8d0bdc6
Shivani-Hiremath <112796703+Shivani-Hiremath@users.noreply.github.com>
2025-04-16 21:22:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://trackmyclass-frontendd.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
