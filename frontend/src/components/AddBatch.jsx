a4f9f599a64cd25be89dcf615757cc720bba70c0
Shivani-Hiremath <112796703+Shivani-Hiremath@users.noreply.github.com>
2025-04-16 22:49:27 +0530

    Update AddBatch.jsx

--- frontend/src/components/AddBatch.jsx
+++ frontend/src/components/AddBatch.jsx
@@ -10,7 +10,8 @@ const AddBatch = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await fetch("http://localhost:5000/batches/add", {
+      await fetch("http://https://trackmyclass-backend.onrender.com
+/batches/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ batchName, batchId, batchFee }),
